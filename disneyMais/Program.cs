8f8a2772833445921ceff99379588b10fa069032
Bruno César Lima Araújo <1561719@sga.pucminas.br>
2025-05-07 22:04:08 -0300

    Primero comit

---
+++ disneyMais/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program {
+    static void Main (string[] args) {
+        Console.WriteLine("Programa iniciado");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++) {
+            string[] producao = Console.ReadLine().Split(';');
+        }
+        int c = int.Parse(Console.ReadLine());
+        for (int i = 0; i < c; i++) {
+            string[] comandos = Console.ReadLine().Split(' ');
+        }
+    }
+}
